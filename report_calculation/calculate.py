90241b3d9cf4f0ce863f0443bfeb59ed074783f3
daniel.ducuara <daniel.ducuara@outscale.com>
2022-11-04 14:09:47 +0000

    launch pre-commit

--- report_calculation/calculate.py
+++ report_calculation/calculate.py
@@ -1,25 +1,29 @@
 from __future__ import annotations
+
+from dataclasses import dataclass
 from typing import Any
 
-from config import binance_client, logger
 from apischema import deserialize
 from binance.exceptions import BinanceAPIException
-from dataclasses import dataclass
+from config import binance_client, logger
 
 from report_calculation.model import Currency
 
-EUR_USDT="EURUSDT"
- 
+EUR_USDT = "EURUSDT"
+
 investment_euros: float = 14802.52
 bank_saving_euros: float = 3520
+
 
 @dataclass
 class CryptoCurrency:
 
-    symbol: str 
-    price: str 
+    symbol: str
+    price: str
+
 
 # Total money on cryptos
+
 
 def total_crypto_usd() -> float:
     logger.info("Calculating total crypto money in usd")
@@ -27,29 +31,37 @@ def total_crypto_usd() -> float:
 
     for currency in Currency.get_all():
         try:
-            crypto_currency: CryptoCurrency = deserialize(CryptoCurrency, binance_client.get_symbol_ticker(symbol=currency.symbol))
+            crypto_currency: CryptoCurrency = deserialize(
+                CryptoCurrency, binance_client.get_symbol_ticker(symbol=currency.symbol)
+            )
             if quantity := currency.quantity:
-                total_usd += float(crypto_currency.price)*quantity
+                total_usd += float(crypto_currency.price) * quantity
         except BinanceAPIException as err:
             logger.exception(f"Error {err} {currency.symbol}")
             raise
     logger.info(f"Total crypto money: {total_usd} usd")
-    return float("{:.2f}".format(total_usd)) 
+    return float("{:.2f}".format(total_usd))
 
 
 def total_crypto_euros() -> float:
     logger.info("Calculating total crypto money in euros")
-    euro_usdt : CryptoCurrency = deserialize(CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT))
-    total_euros = total_crypto_usd()/float(euro_usdt.price)
-    logger.info(f"Total crypto money: {total_euros} euros") 
+    euro_usdt: CryptoCurrency = deserialize(
+        CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT)
+    )
+    total_euros = total_crypto_usd() / float(euro_usdt.price)
+    logger.info(f"Total crypto money: {total_euros} euros")
     return float("{:.2f}".format(total_euros))
+
 
 # Total money (total money on cryptos + bank savings)
 
+
 def total_usd() -> float:
     logger.info("Calculating total money in usd (total crypto money + bank savings)")
-    euro_usdt : CryptoCurrency = deserialize(CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT))
-    total = total_crypto_usd() + bank_saving_euros*float(euro_usdt.price)
+    euro_usdt: CryptoCurrency = deserialize(
+        CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT)
+    )
+    total = total_crypto_usd() + bank_saving_euros * float(euro_usdt.price)
     logger.info(f"Total money: {total} usd")
     return float("{:.2f}".format(total))
 
@@ -60,12 +72,16 @@ def total_euros() -> float:
     logger.info(f"Total money: {total} euro")
     return float("{:.2f}".format(total))
 
+
 # Total profit (total money on cryptos - investment)
+
 
 def profit_usd() -> float:
     logger.info("Calculating total profit in usd (total crypto money - investment)")
-    euro_usdt : CryptoCurrency = deserialize(CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT))
-    diff = total_crypto_usd() - investment_euros*float(euro_usdt.price)
+    euro_usdt: CryptoCurrency = deserialize(
+        CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT)
+    )
+    diff = total_crypto_usd() - investment_euros * float(euro_usdt.price)
     logger.info(f"Total profit: {diff} usd")
     return float("{:.2f}".format(diff))
 
@@ -76,18 +92,25 @@ def profit_euros() -> float:
     logger.info(f"Total profit: {diff} euros")
     return float("{:.2f}".format(diff))
 
+
 # Total invested money
+
 
 def invested_usd() -> float:
     logger.info("Calculating total investment in usd")
-    euro_usdt : CryptoCurrency = deserialize(CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT))
-    return float("{:.2f}".format(investment_euros*float(euro_usdt.price)))
+    euro_usdt: CryptoCurrency = deserialize(
+        CryptoCurrency, binance_client.get_symbol_ticker(symbol=EUR_USDT)
+    )
+    return float("{:.2f}".format(investment_euros * float(euro_usdt.price)))
+
 
 def invested_euros() -> float:
     logger.info("Calculating total investment in euros")
     return float("{:.2f}".format(investment_euros))
 
+
 # update Crypto
+
 
 def update(symbol: str, quantity: str) -> dict[str, Any]:
     logger.info(f"Updating {symbol} with value {quantity}")
@@ -95,15 +118,19 @@ def update(symbol: str, quantity: str) -> dict[str, Any]:
     logger.info(f"Updated {result}")
     return result
 
+
 # get crypto
 
-def read(symbol:str) -> dict[str, Any]:
+
+def read(symbol: str) -> dict[str, Any]:
     logger.info(f"Reading {symbol} data")
     result = (Currency.get_by_id(symbol)).to_dict()
     logger.info(f"Result {result}")
     return result
 
+
 # create crypto
+
 
 def create(symbol: str, quantity: str) -> dict[str, Any]:
     logger.info(f"Adding {symbol} with value {quantity}")
@@ -111,13 +138,12 @@ def create(symbol: str, quantity: str) -> dict[str, Any]:
     logger.info(f"Added {result}")
     return result
 
-# delete crypto 
+
+# delete crypto
+
 
 def delete(symbol: str) -> dict[str, Any]:
     logger.info(f"Deleting {symbol} data")
     result = (Currency.get_by_id(symbol).delete()).to_dict()
     logger.info(f"Deleted {result}")
     return result
-
-
-
