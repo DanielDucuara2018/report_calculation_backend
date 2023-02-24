dfcd2644e04bca39a3cc5b7a9766671fa3b9418d
daniel.ducuara <daniel.ducuara@outscale.com>
2023-02-24 16:17:43 +0000

    Move calculate actions and add api CRUD

--- report_calculation/actions/calculate.py
+++ report_calculation/actions/calculate.py
@@ -1,21 +1,17 @@
 from __future__ import annotations
 
 import logging
-from typing import Optional, Union
 
 from report_calculation.model import CurrencyPair as ModelCurrencyPair
-from report_calculation.utils import (
-    async_get_symbol_ticker,
-    async_get_symbol_tickers,
-    get_symbol_ticker,
-)
+from report_calculation.model import User as ModelUser
+from report_calculation.utils import async_get_symbol_ticker, async_get_symbol_tickers
 
 logger = logging.getLogger(__name__)
 
 EUR_USDT = "EURUSDT"
 
-investment_euros: float = 16302.52
-bank_saving_euros: float = 3020
+investment_euros: float = 16802.52
+bank_saving_euros: float = 2520
 
 
 # Total money on cryptos
@@ -115,50 +111,3 @@ async def invested_euros() -> float:
     investment = float("{:.2f}".format(investment_euros))
     logger.info("Investment: %f euros", investment)
     return investment
-
-
-# update Crypto
-
-
-def update(symbol: str, quantity: str) -> ModelCurrencyPair:
-    logger.info("Updating %s with value %s", symbol, quantity)
-    result = ModelCurrencyPair.get(symbol).update(quantity=float(quantity))  # type: ignore
-    logger.info("Result %s", result)
-    return result
-
-
-# get crypto
-
-
-def read(
-    symbol: Optional[str] = None,
-) -> Union[ModelCurrencyPair, list[ModelCurrencyPair]]:
-    if symbol:
-        logger.info("Reading %s data", symbol)
-        result = ModelCurrencyPair.get(symbol)
-    else:
-        logger.info("Reading all data")
-        result = ModelCurrencyPair.get()
-    logger.info("Result %s", result)
-    return result
-
-
-# create crypto
-
-
-def create(symbol: str, quantity: str) -> ModelCurrencyPair:
-    logger.info("Adding %s with value %s", symbol, quantity)
-    get_symbol_ticker(symbol)
-    result = ModelCurrencyPair(symbol=symbol, quantity=float(quantity)).create()
-    logger.info("Added %s", result)
-    return result
-
-
-# delete crypto
-
-
-def delete(symbol: str) -> ModelCurrencyPair:
-    logger.info("Deleting %s data", symbol)
-    result = ModelCurrencyPair.get(symbol).delete()  # type: ignore
-    logger.info("Deleted %s", result)
-    return result
---
+++ report_calculation/api/routers/calculate.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+from fastapi import APIRouter
+
+from report_calculation.actions.calculate import (
+    invested_euros,
+    invested_usd,
+    profit_euros,
+    profit_usd,
+    total_crypto_euros,
+    total_crypto_usd,
+    total_euros,
+    total_usd,
+)
+
+router = APIRouter(
+    prefix="/calculate",
+    tags=["calculate"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+## Calculate
+
+
+class CalculateActions(str, Enum):
+    TOTAL_USD = "total_usd"
+    TOTAL_EUROS = "total_euros"
+    TOTAL_CRYPTO_USD = "total_crypto_usd"
+    TOTAL_CRYPTO_EUROS = "total_crypto_euros"
+    PROFIT_EUROS = "profit_euros"
+    PROFIT_USD = "profit_usd"
+    INVESTED_USD = "invested_usd"
+    INVESTED_EUROS = "invested_euros"
+
+
+calculate_action = {
+    CalculateActions.TOTAL_USD: total_usd,
+    CalculateActions.TOTAL_EUROS: total_euros,
+    CalculateActions.TOTAL_CRYPTO_USD: total_crypto_usd,
+    CalculateActions.TOTAL_CRYPTO_EUROS: total_crypto_euros,
+    CalculateActions.PROFIT_USD: profit_usd,
+    CalculateActions.PROFIT_EUROS: profit_euros,
+    CalculateActions.INVESTED_USD: invested_usd,
+    CalculateActions.INVESTED_EUROS: invested_euros,
+}
+
+
+@router.get("/{action}")
+async def calculate_total_usd(action: CalculateActions) -> float:
+    return await calculate_action.get(action)()
