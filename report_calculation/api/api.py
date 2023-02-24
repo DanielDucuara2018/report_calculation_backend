fdc197cefe11670d9d3a86b6a26f18a135b44d76
daniel.ducuara <daniel.ducuara@outscale.com>
2023-02-24 15:59:41 +0000

    Add routers fastapi

--- report_calculation/api/api.py
+++ report_calculation/api/api.py
@@ -1,46 +1,13 @@
-from report_calculation.calculate import create, delete, read, update
+from report_calculation.api.routers.calculate import router as calculate_router
+from report_calculation.api.routers.currency import router as currency_router
+from report_calculation.api.routers.user import router as user_router
 from report_calculation.config import app
-from report_calculation.schema import CurrencyPair as SchemaCurrencyPair
+
+app.include_router(user_router)
+app.include_router(currency_router)
+app.include_router(calculate_router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to report_calculation app"}
-
-
-## Currencies
-# add new crypto in database
-@app.post("/currencies/{symbol}")
-async def create_currency(symbol: str, quantity: str) -> SchemaCurrencyPair:
-    return create(symbol, quantity)
-
-
-# get crypto data
-@app.get("/currencies/{symbol}")
-async def read_currency(symbol: str) -> SchemaCurrencyPair:
-    return read(symbol)  # type: ignore
-
-
-@app.get("/currencies")
-async def read_currencies() -> list[SchemaCurrencyPair]:
-    return read()  # type: ignore
-
-
-# update crypto data
-@app.put("/currencies/{symbol}")
-async def update_currency(symbol: str, quantity: str) -> SchemaCurrencyPair:
-    return update(symbol, quantity)
-
-
-# delete existing from db
-@app.delete("/currencies/{symbol}")
-async def delete_currency(symbol: str) -> SchemaCurrencyPair:
-    return delete(symbol)
-
-
-## Calculate
-
-
-@app.get("calculate/{method}")
-async def calculate(method: str):
-    pass
