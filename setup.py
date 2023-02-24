9257cf0fa36dd3236544f4df3ea3feab84a70d16
daniel.ducuara <daniel.ducuara@outscale.com>
2023-02-24 15:52:27 +0000

    Add dev section in setup file

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ INSTALL_REQUIRES = [
     "SQLAlchemy==1.4.37",
     "psycopg2==2.9.1",
     "configparser==5.3.0",
-    "fastapi[all]",
-    "asyncio",
+    "fastapi[all]==0.92.0",
+    "asyncio==3.4.3",
 ]
 
 setup(
@@ -18,7 +18,6 @@ setup(
     version=VERSION,
     python_requires=">=3.9.0",
     packages=find_packages(exclude=["tests"]),
-    # url="http://www.report_calulation.com",
     author="Daniel Ducuara",
     author_email="daniel14015@gmail.com",
     description="Get a report of my porfolio",
@@ -31,11 +30,23 @@ setup(
     },
     install_requires=INSTALL_REQUIRES,
     extras_require={
+        "dev": [
+            "alembic==1.9.4",
+            "bandit==1.7.0",
+            "mypy==0.931",
+            "pdbpp==0.10.2",
+            "pre-commit==2.10.1",
+            "pylint==2.7.0",
+            "types-requests==2.27.11",
+            "types-emoji==1.2.7",
+            "black==22.3.0",
+            "isort==5.10.1",
+        ],
         "test": [
             "pytest==6.2.4",
             "pytest-mock==3.6.1",
             "pytest-cov==2.12.1",
             "pytest-asyncio==0.15.1",
-        ]
+        ],
     },
 )
