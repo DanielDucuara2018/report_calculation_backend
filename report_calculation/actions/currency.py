6f2bb5e45ce4075ff8bf7ffca81207f70655e0ac
daniel.ducuara <daniel.ducuara@outscale.com>
2023-02-28 16:15:33 +0000

    Use user in all currency actions

--- report_calculation/actions/currency.py
+++ report_calculation/actions/currency.py
@@ -11,10 +11,14 @@ logger = logging.getLogger(__name__)
 # create crypto
 
 
-def create(symbol: str, quantity: str) -> ModelCurrencyPair:
-    logger.info("Adding %s with value %s", symbol, quantity)
+def create(
+    user_id: str, symbol: str, quantity: Optional[Union[str, float]] = None
+) -> ModelCurrencyPair:
+    logger.info("Adding %s with value %s for user %s", symbol, quantity, user_id)
     get_symbol_ticker(symbol)
-    result = ModelCurrencyPair(symbol=symbol, quantity=float(quantity)).create()
+    result = ModelCurrencyPair(
+        symbol=symbol, quantity=float(quantity or str(0)), user_id=user_id
+    ).create()
     logger.info("Added %s", result)
     return result
 
@@ -22,9 +26,9 @@ def create(symbol: str, quantity: str) -> ModelCurrencyPair:
 # delete crypto
 
 
-def delete(symbol: str) -> ModelCurrencyPair:
-    logger.info("Deleting %s data", symbol)
-    result = ModelCurrencyPair.get(symbol).delete()  # type: ignore
+def delete(user_id: str, symbol: str) -> ModelCurrencyPair:
+    logger.info("Deleting %s data for user %s", symbol, user_id)
+    result = ModelCurrencyPair.get(user_id=user_id, symbol=symbol).delete()  # type: ignore
     logger.info("Deleted %s", result)
     return result
 
@@ -33,14 +37,15 @@ def delete(symbol: str) -> ModelCurrencyPair:
 
 
 def read(
+    user_id: str,
     symbol: Optional[str] = None,
 ) -> Union[ModelCurrencyPair, list[ModelCurrencyPair]]:
     if symbol:
-        logger.info("Reading %s data", symbol)
-        result = ModelCurrencyPair.get(symbol)
+        logger.info("Reading %s data for user %s", symbol, user_id)
+        result = ModelCurrencyPair.get(user_id=user_id, symbol=symbol)
     else:
-        logger.info("Reading all data")
-        result = ModelCurrencyPair.get()
+        logger.info("Reading all data for user %s", user_id)
+        result = ModelCurrencyPair.find(user_id=user_id)
     logger.info("Result %s", result)
     return result
 
@@ -48,8 +53,10 @@ def read(
 # update Crypto
 
 
-def update(symbol: str, quantity: str) -> ModelCurrencyPair:
-    logger.info("Updating %s with value %s", symbol, quantity)
-    result = ModelCurrencyPair.get(symbol).update(quantity=float(quantity))  # type: ignore
+def update(
+    user_id: str, symbol: str, quantity: Optional[float] = None
+) -> ModelCurrencyPair:
+    logger.info("Updating %s with value %s for user %s", symbol, quantity, user_id)
+    result = ModelCurrencyPair.get(user_id=user_id, symbol=symbol).update(quantity=float(quantity or str(0)))  # type: ignore
     logger.info("Result %s", result)
     return result
