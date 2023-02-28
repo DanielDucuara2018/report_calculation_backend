de9ce423ae7e2af8bc3d11cd9f58f53cb84e4097
daniel.ducuara <daniel.ducuara@outscale.com>
2023-02-28 16:24:46 +0000

    Rename User to SchemaUserReturn and SchemaUserRequest

--- report_calculation/api/routers/user.py
+++ report_calculation/api/routers/user.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from report_calculation.actions.user import create, delete, read, update
-from report_calculation.schema import User
+from report_calculation.schema import UserRequest as SchemaUserRequest
+from report_calculation.schema import UserResponse as SchemaUserResponse
 
 router = APIRouter(
     prefix="/users",
@@ -12,28 +13,28 @@ router = APIRouter(
 ## Users
 # add new user in database
 @router.post("/")
-async def create_user(user: User) -> User:
+async def create_user(user: SchemaUserRequest) -> SchemaUserResponse:
     return create(user)
 
 
 # get crypto data
 @router.get("/{user_id}")
-async def read_user(user_id: str):
+async def read_user(user_id: str) -> SchemaUserResponse:
     return read(user_id)
 
 
 @router.get("/")
-async def read_user():
+async def read_user() -> list[SchemaUserResponse]:
     return read()
 
 
 # update crypto data
 @router.put("/{user_id}")
-async def update_user(user_id: str, data: User):
+async def update_user(user_id: str, data: SchemaUserRequest) -> SchemaUserResponse:
     return update(user_id, data)
 
 
 # delete existing from db
 @router.delete("/{user_id}")
-async def delete_currency(user_id: str):
-    delete(user_id)
+async def delete_currency(user_id: str) -> SchemaUserResponse:
+    return delete(user_id)
