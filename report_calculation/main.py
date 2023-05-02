2359890e909c67aebbcf86bdb1c861737eff527f
daniel.ducuara <daniel.ducuara@outscale.com>
2023-05-02 18:31:58 +0000

    Add automatic migration apply

--- report_calculation/main.py
+++ report_calculation/main.py
@@ -1,9 +1,11 @@
 from report_calculation.bot import telegram_app
 from report_calculation.config import logger
+from report_calculation.db import initialize
 
 
 def main():
     logger.info("Initialising App")
+    initialize(True)
     telegram_app.run_polling()
 
 
