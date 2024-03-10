662f06fa410756573d16b866173a9245db1cad42
daniel.ducuara <daniel.ducuara@outscale.com>
2024-03-10 22:51:09 +0000

    Addes user_name, password and email to pydantic model + remove currency_pairs and purchases

--- report_calculation/schema/user.py
+++ report_calculation/schema/user.py
@@ -10,12 +10,13 @@ class UserRequest(BaseModel):
     investment_euros: Optional[float] = None
     savings_euros: Optional[float] = None
     description: Optional[str] = None
+    user_name: Optional[str] = None
+    password: Optional[str] = None
+    email: Optional[str] = None
 
 
 class UserResponse(UserRequest):
 
     user_id: Optional[str] = None
-    currency_pairs: Optional[list] = None
-    purchases: Optional[list] = None
     update_date: Optional[datetime] = None
     creation_date: Optional[datetime] = None
